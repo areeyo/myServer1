d59f521ca9094657b1cea35c777895f4c13640e6
areeyo <yakeemo2@gmail.com>
2014-11-05 17:49:49 +0700

    8th commit use table from server

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ var app = express()
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/api/book',function(req,res){
+	var books = [
+		{title: 'Strange', price: 100},
+		{title: 'Agu', price: 100},
+		{title: 'Int', price: 100}
+	];
+	res.send(books);
+})
+
 var server = app.listen(3000, function () {
 
   	console.log("server is running")
--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,7 @@
 angular.module("myApp", [])
-.controller('mainCtrl', function($scope){
-	$scope.books = [
-		{title: 'Strange', price: 100},
-		{title: 'Agi', price: 100},
-		{title: 'Int', price: 100}
-	];
+.controller('mainCtrl', function($scope,$http){
+	$scope.books = [];
+	$http.get('/api/book').success(function(data){
+		$scope.books=data;
+	})
 })
