2aa72a37b898cbf02260ada6a6f9cbc5ee0d5759
areeyo <yakeemo2@gmail.com>
2014-11-10 19:07:21 +0700

    12th insert book to database

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ var express = require('express')
 var mongojs = require('mongojs')
 var app = express()
 var db = mongojs('my_server', ['book']);
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 
 app.get('/api/book',function(req,res){
@@ -12,6 +14,11 @@ app.get('/api/book',function(req,res){
 	});
 });
 
+app.post('/api/book',function(req,res){
+	db.book.insert((req.body),function(err,books){
+		res.send(books);	
+	});
+});
 
 var server = app.listen(3000, function () {
 
